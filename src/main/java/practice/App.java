6df166d4d172d9e1feff7de1d801f0c91651dcd8
Santosh JS <santoshjsutar@gmail.com>
2026-06-16 12:53:28 +0530

    Coding practice

--- src/main/java/practice/App.java
+++ src/main/java/practice/App.java
@@ -1,12 +1,45 @@
 package practice;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
 
   
+
+        int[] num = {1,3,4,5,6,7};
+        int target = 5;
+       int[] result = twoSum(num ,target);
+
+       System.out.println(Arrays.toString(result));
+
+
      
-     
+    }
+
+    public static int [] twoSum(int[] num ,int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        
+
+        for ( int i =0 ; i < num.length; i++) {
+
+             int complement = target - num[i];
+
+            if ( map.containsKey(complement)) {
+
+                return new int[] {map.get(complement) , i};
+            }
+             map.put(num[i], i);
+
+
+        }
+        return new int[] {};
+
+   
 
 
     }
