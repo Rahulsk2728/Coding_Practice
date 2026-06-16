1fe7ed54a4bc72167fd1b4b060b962e1c1aaee8f
Rahulsk2728 <kshirsagar.rahul97@gmail.com>
2026-06-16 14:57:47 +0530

    New code

---
+++ src/main/java/Reverse.java
@@ -0,0 +1,31 @@
+public class Reverse {
+
+     public static void main(String[] args) {
+
+
+
+      System.out.println(reverseWords("This is it by Micheal jackson"));
+
+
+     }
+
+     public static String reverseWords( String sentence) {
+
+      String [] revWords = sentence.trim().split("\\s+");
+
+      StringBuilder revresed = new StringBuilder();
+
+      for ( int i= revWords.length-1; i>=0;i--) {
+
+        revresed= revresed.append(revWords[i]);
+        if( i >=0) {
+            revresed.append(" ");
+        }
+      }
+   return revresed.toString();
+
+
+     } 
+
+    
+}
